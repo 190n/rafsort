144c52f3440df68d0a3bfc78fdc4be582d664d10
Ben Grant <benjamin.j.grant@gmail.com>
2021-06-09 13:07:39 -0700

    Source code link

--- controls.js
+++ controls.js
@@ -51,81 +51,90 @@ function Controls(props) {
     }
 
     return html`
-        <div>
-            <h1>rafsort</h1>
-            <p>
-                <label for="n-rafs">
-                    Number of Rafs: ${nRafs}
-                    <input
-                        id="n-rafs"
-                        type="range"
-                        min="5"
-                        max="50"
-                        step="1"
-                        value=${nRafs}
-                        onchange=${e => setNRafs(parseInt(e.target.value))}
-                    />
-                </label>
-                <label for="array-type">
-                    Array type:
-                    <select onchange=${e => setArrayType(e.target.value)}>
-                        ${Object.keys(arrayTypes).map(t => html`
-                            <option value=${t} selected=${t == arrayType}>${arrayTypes[t]}</option>
-                        `)}
+        <main>
+            <header>
+                <h1>rafsort</h1>
+            </header>
+            <section id="controls">
+                <p>
+                    <label for="n-rafs">
+                        Number of Rafs: ${nRafs}
+                        <input
+                            id="n-rafs"
+                            type="range"
+                            min="5"
+                            max="50"
+                            step="1"
+                            value=${nRafs}
+                            onchange=${e => setNRafs(parseInt(e.target.value))}
+                        />
+                    </label>
+                    <label for="array-type">
+                        Array type:
+                        <select onchange=${e => setArrayType(e.target.value)}>
+                            ${Object.keys(arrayTypes).map(t => html`
+                                <option value=${t} selected=${t == arrayType}>${arrayTypes[t]}</option>
+                            `)}
+                        </select>
+                    </label>
+                </p>
+                <p>
+                    <label for="swap-delay">
+                        Swap delay: ${swapDelay}ms
+                        <input
+                            id="swap-delay"
+                            type="range"
+                            min="0"
+                            max="500"
+                            step="25"
+                            value=${swapDelay}
+                            onchange=${e => setSwapDelay(parseInt(e.target.value))}
+                        />
+                    </label>
+                    <label for="compare-delay">
+                        Comparison delay: ${compareDelay}ms
+                        <input
+                            id="compare-delay"
+                            type="range"
+                            min="0"
+                            max="500"
+                            step="25"
+                            value=${compareDelay}
+                            onchange=${e => setCompareDelay(parseInt(e.target.value))}
+                        />
+                    </label>
+                    <label for="extra-delay">
+                        Additional delay: ${extraDelay}ms
+                        <input
+                            id="extra-delay"
+                            type="range"
+                            min="0"
+                            max="500"
+                            step="25"
+                            value=${extraDelay}
+                            onchange=${e => setExtraDelay(parseInt(e.target.value))}
+                        />
+                    </label>
+                </p>
+                <p>
+                    Sorting algorithm:
+                    <select>
+                        <option>Bubble sort</option>
+                        <option>sike you thought!</option>
                     </select>
-                </label>
-            </p>
-            <p>
-                <label for="swap-delay">
-                    Swap delay: ${swapDelay}ms
-                    <input
-                        id="swap-delay"
-                        type="range"
-                        min="0"
-                        max="500"
-                        step="25"
-                        value=${swapDelay}
-                        onchange=${e => setSwapDelay(parseInt(e.target.value))}
-                    />
-                </label>
-                <label for="compare-delay">
-                    Comparison delay: ${compareDelay}ms
-                    <input
-                        id="compare-delay"
-                        type="range"
-                        min="0"
-                        max="500"
-                        step="25"
-                        value=${compareDelay}
-                        onchange=${e => setCompareDelay(parseInt(e.target.value))}
-                    />
-                </label>
-                <label for="extra-delay">
-                    Additional delay: ${extraDelay}ms
-                    <input
-                        id="extra-delay"
-                        type="range"
-                        min="0"
-                        max="500"
-                        step="25"
-                        value=${extraDelay}
-                        onchange=${e => setExtraDelay(parseInt(e.target.value))}
-                    />
-                </label>
-            </p>
-            <p>
-                Sorting algorithm:
-                <select>
-                    <option>Bubble sort</option>
-                    <option>sike you thought!</option>
-                </select>
-            </p>
-            <p>
-                <button id="create" onclick=${onCreate}>Create Rafs</button>
-                <button id="sort" onclick=${onSort}>Sort Rafs</button>
-            </p>
-            <div ref=${containerRef} id="rafs"></div>
-        </div>`;
+                </p>
+                <p>
+                    <button id="create" onclick=${onCreate}>Create Rafs</button>
+                    <button id="sort" onclick=${onSort}>Sort Rafs</button>
+                </p>
+            </section>
+            <section id="rafs-wrapper">
+                <div ref=${containerRef} id="rafs"></div>
+            </section>
+            <footer>
+                <a href="https://github.com/190n/rafsort" target="_blank">Source code</a>
+            </footer>
+        </main>`;
 }
 
 render(html`<${Controls} />`, document.body);
